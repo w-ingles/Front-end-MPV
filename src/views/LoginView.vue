6c42712d099fc1d215f0786dafe13aa0c76eca46
w-ingles <wingles@hotmail.com>
2024-07-28 21:28:42 -0300

    Modificando page de login e rotas

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,17 @@
 <template>
-
+aaa
 </template>
 
-<script setup>
-
+<script >
+export default {
+  name: 'LoginView',
+  props: {
+    msg: String
+  },
+  mounted() {
+    alert(import.meta.env.VITE_API_URL)
+  },
+}
 </script>
 
 <style scoped>
